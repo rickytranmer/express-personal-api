21f3d4fbb88243117a7238efd26568ce63a37a19
rickytranmer <rwtranmer@gmail.com>
2018-01-10 13:52:20 -0700

    mongo connection on heroku

--- db/seeds.js
+++ db/seeds.js
@@ -1,5 +1,9 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/personal-api');
+mongoose.connect( process.env.MONGODB_URI || 
+                  process.env.MONGOLAB_URI || 
+                  process.env.MONGOLAB_SILVER_URI || 
+                  process.env.MONGOHQ_URL || 
+                  "mongodb://localhost/personal-api");
 
 // - Wipe db
 var Concert = require('../models/concert');
--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ var mongo = require('mongodb').MongoClient;
 var mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI || 
                   process.env.MONGOLAB_URI || 
+                  process.env.MONGOLAB_SILVER_URI || 
                   process.env.MONGOHQ_URL || 
                   "mongodb://localhost/personal-api");
 
